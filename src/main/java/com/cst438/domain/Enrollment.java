fe940b7c0fb2ff67b3c19bcecd79d62e7714e5bf
Kenneth Ao <kao@csumb.edu>
2024-03-08 22:50:03 -0800

    completed enrollment JPA

--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -2,6 +2,8 @@ package com.cst438.domain;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 public class Enrollment {
     @Id
@@ -11,7 +13,49 @@ public class Enrollment {
 	
 	// TODO complete this class
     // add additional attribute for grade
+    private String grade;
+
     // create relationship between enrollment and user entities
+    @ManyToOne
+    @JoinColumn(name="id")
+    private User user;
+
     // create relationship between enrollment and section entities
+    @OneToMany(mappedBy="sec_id")
+    private List<Section> sections;
+
     // add getter/setter methods
+
+
+    public int getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public void setEnrollmentId(int enrollmentId) {
+        this.enrollmentId = enrollmentId;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
+    }
 }
